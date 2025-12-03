054594d9f7f457d789a4fab29d75ac5201223019
NithinKrishna-65 <nithinkrishna.2202148@srec.ac.in>
2025-12-03 07:34:56 +0530

    Create Factors.java

---
+++ Basic Programs/Factors.java
@@ -0,0 +1,22 @@
+//Given a number N, print its factors.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        boolean first = true;
+
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                if (first) {
+                    System.out.print(i);   
+                    first = false;
+                } else {
+                    System.out.print(" " + i);  
+                }
+            }
+        }
+    }
+}
